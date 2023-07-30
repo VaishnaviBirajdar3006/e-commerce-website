5dcc100e6f6e9da30fa8d9fcd9ad50de1910f1fb
Akash <apdua453@gmail.com>
2023-07-30 19:26:36 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navigation from "./Components/Navigation/Navigation";
+import "./global.css";
+
+function App() {
+  return (
+    <>
+      <Navigation />
+      <p>Hello World</p>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import "./Header.scss";
+
+export default function Header() {
+  return (
+    <>
+      <header>
+        <section className="header-main">
+          <div>
+            <div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+          </div>
+        </section>
+      </header>
+    </>
+  );
+}
